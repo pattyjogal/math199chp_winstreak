5feba86c8df5d8633d79a9e93c9f525353a36c33
Patrick Gallagher <patrickj@cpgallagher.com>
2017-09-25 16:20:51 -0500

    Updated readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 What teams in sports have the greatest home team advantage? This is an investigation into that
 problem, implemented in Python and drawing from the [ESPN/MLB] API.
+
+### To make use of the program, make sure to install the dependencies with
+
+`pip install -r requirements.txt`
--- analysis.py
+++ analysis.py
@@ -1,18 +1,16 @@
 from pprint import pprint
 import datetime
 import requests
-from bs4 import BeautifulSoup
 
-### MLB ANALYSIS ###
+### MLB DATA ###
 
 import mlbgame
-
 
 
 # Get 10 years of data!
 
 def get_mlb_data():
-    data_years = [datetime.date(2005 + n, 9, 25) for n in range(0,10)]
+    data_years = [datetime.date(2005 + n, 9, 25) for n in range(0, 10)]
 
     # This is the main dict which holds data for each year
     standings_by_year = {}
@@ -24,21 +22,59 @@ def get_mlb_data():
                 mlb_teams.append(mlbgame.info.Team(team))
 
         team_standings = [{
-                team.team_short: {
-                    'away_wins': int(team.away.split('-')[0]),
-                    'away_losses': int(team.away.split('-')[1]),
-                    'home_wins': int(team.home.split('-')[0]),
-                    'home_losses': int(team.home.split('-')[1]),
-                }
-            } for team in mlb_teams]
+            team.team_short: {
+                'away_wins': int(team.away.split('-')[0]),
+                'away_losses': int(team.away.split('-')[1]),
+                'home_wins': int(team.home.split('-')[0]),
+                'home_losses': int(team.home.split('-')[1]),
+            }
+        } for team in mlb_teams]
 
         standings_by_year.update({year.year: team_standings})
+    return standings_by_year
 
-def get_nhl_data():
-    years = [str(year) for year in range(2005, 2016)]
-    for year in years: 
-        html = requests.get("https://www.nhl.com/standings/" + year).content.decode('utf-8','ignore') 
-        soup = BeautifulSoup(html, 'html.parser')
-        print(soup.findAll("div"))
 
-get_nhl_data()
+pprint(get_mlb_data())
+
+# from nhlscrapi.games.game import Game, GameType, GameKey
+
+# def get_nhl_data():
+#     years = [year for year in range(2008, 2016)]
+#     for year in years:
+#         gt = GameType.Regular
+#         game_key = GameKey(year, 1226, gt)
+#         game = Game(game_key)
+#         pprint(game.cum_stats)
+# get_nhl_data()
+
+from nba_py import team, constants
+import itertools
+
+
+### NBA DATA ###
+
+def get_nba_data():
+    years = [year for year in range(2008, 2016)]
+    standings = {}
+    for year in years:
+        year_standings = {}
+        for _, team_data in constants.TEAMS.items():
+            team_id = team_data['id']
+            team_record = {}
+            team_record.update({
+                team_data['name']: {
+                    x: y for (x, y) in zip(
+                        ["away_losses", "away_wins", "home_losses", "home_wins"],
+                        itertools.chain(*[
+                            [str(x['GP']) for x in team.TeamGeneralSplits(team_id, location=loc, season=str(
+                                year) + '-' + "%02d" % ((year + 1) % 100)).wins_losses()[::-1]]
+                            for loc in ["Road", "Home"]
+                        ])
+                    )
+                }
+            })
+            year_standings.update(team_record)
+        standings.update({year: year_standings})
+    return standings
+
+pprint(get_nba_data())
