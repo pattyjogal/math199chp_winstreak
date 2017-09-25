7a41840b69573b9c5ff0aa6dbd93a135b5fa2c62
Patrick Gallagher <patrickj@cpgallagher.com>
2017-09-25 01:33:12 -0500

    Added functions and started NHL data collection

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import datetime
-
+import requests
+from bs4 import BeautifulSoup
 
 ### MLB ANALYSIS ###
 
@@ -9,26 +10,35 @@ import mlbgame
 
 
 # Get 10 years of data!
-data_years = [datetime.date(2005 + n, 9, 25) for n in range(0,10)]
 
-# This is the main dict which holds data for each year
-standings_by_year = {}
+def get_mlb_data():
+    data_years = [datetime.date(2005 + n, 9, 25) for n in range(0,10)]
 
-for year in data_years:
-    mlb_teams = []
-    for division in mlbgame.info.standings(year)['divisions']:
-        for team in division['teams']:
-            mlb_teams.append(mlbgame.info.Team(team))
+    # This is the main dict which holds data for each year
+    standings_by_year = {}
 
-    team_standings = [{
-            team.team_short: {
-                'away_wins': int(team.away.split('-')[0]),
-                'away_losses': int(team.away.split('-')[1]),
-                'home_wins': int(team.home.split('-')[0]),
-                'home_losses': int(team.home.split('-')[1]),
-            }
-        } for team in mlb_teams]
+    for year in data_years:
+        mlb_teams = []
+        for division in mlbgame.info.standings(year)['divisions']:
+            for team in division['teams']:
+                mlb_teams.append(mlbgame.info.Team(team))
 
-    standings_by_year.update({year.year: team_standings})
+        team_standings = [{
+                team.team_short: {
+                    'away_wins': int(team.away.split('-')[0]),
+                    'away_losses': int(team.away.split('-')[1]),
+                    'home_wins': int(team.home.split('-')[0]),
+                    'home_losses': int(team.home.split('-')[1]),
+                }
+            } for team in mlb_teams]
 
-pprint(standings_by_year)
+        standings_by_year.update({year.year: team_standings})
+
+def get_nhl_data():
+    years = [str(year) for year in range(2005, 2016)]
+    for year in years: 
+        html = requests.get("https://www.nhl.com/standings/" + year).content.decode('utf-8','ignore') 
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.findAll("div"))
+
+get_nhl_data()
