7d7077938bc44e76e567440641f8520e0d133fb5
Patrick Gallagher <patrickj@cpgallagher.com>
2017-09-27 01:56:45 -0500

    Finished NHL data scraping

--- analysis.py
+++ analysis.py
@@ -34,22 +34,45 @@ def get_mlb_data():
     return standings_by_year
 
 
-pprint(get_mlb_data())
+# pprint(get_mlb_data())
 
-# from nhlscrapi.games.game import Game, GameType, GameKey
+from bs4 import BeautifulSoup
 
-# def get_nhl_data():
-#     years = [year for year in range(2008, 2016)]
-#     for year in years:
-#         gt = GameType.Regular
-#         game_key = GameKey(year, 1226, gt)
-#         game = Game(game_key)
-#         pprint(game.cum_stats)
-# get_nhl_data()
+def get_nhl_data():
+    years = [str(year) for year in range(2008, 2016)]
+    nhl_data = {}
+    for year in years:
+        year_data = {}
+        html = requests.get("http://www.espn.com/nhl/standings/_/year/{}/seasontype/2".format(year)).content
+        soup = BeautifulSoup(html, "lxml")
+        table = soup.find('table', {'class': 'tablehead'})
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [elem.text.strip() for elem in cols if len(cols) > 11]
+            if cols and cols[9] != "HOME":
+                hwins, hlosses, hties = [int(x) for x in cols[9].split('-')]
+                awins, alosses, aties = [int(x) for x in cols[10].split('-')]
+                team_name = cols[0][4:]
+                year_data.update({
+                    team_name: {
+                        'away_wins': awins,
+                        'away_losses': alosses,
+                        'away_ties': aties,
+                        # Ok so apparently a tie counts as a 1/2 win
+                        'away_percent_wins': (awins + aties / 2) / (awins + alosses + aties),
+                        'home_wins': hwins,
+                        'home_losses': hlosses,
+                        'home_ties': hties,
+                        'home_percent_wins': (hwins + hties / 2) / (hwins + hlosses + hties),
+                        }
+                    })
+        nhl_data.update({year: year_data})
+    return nhl_data
+
+pprint(get_nhl_data())
 
 from nba_py import team, constants
-import itertools
-
 
 ### NBA DATA ###
 
@@ -61,20 +84,25 @@ def get_nba_data():
         for _, team_data in constants.TEAMS.items():
             team_id = team_data['id']
             team_record = {}
+            away_score_data = team.TeamGeneralSplits(team_id, location="Road", season=str(year) + '-' + "%02d" % ((year + 1) % 100)).wins_losses()
+            awins, alosses = [x['GP'] for x in away_score_data]
+            home_score_data = team.TeamGeneralSplits(team_id, location="Home", season=str(year) + '-' + "%02d" % ((year + 1) % 100)).wins_losses()
+            hwins, hlosses = [x['GP'] for x in home_score_data]
             team_record.update({
                 team_data['name']: {
-                    x: y for (x, y) in zip(
-                        ["away_losses", "away_wins", "home_losses", "home_wins"],
-                        itertools.chain(*[
-                            [str(x['GP']) for x in team.TeamGeneralSplits(team_id, location=loc, season=str(
-                                year) + '-' + "%02d" % ((year + 1) % 100)).wins_losses()[::-1]]
-                            for loc in ["Road", "Home"]
-                        ])
-                    )
+                    'away_wins': awins,
+                    'away_losses': alosses,
+                    'away_percent_wins': awins / (awins + alosses),
+                    'home_wins': hwins,
+                    'home_losses': hlosses,
+                    'home_percent_wins': hwins / (hwins + hlosses),
                 }
             })
             year_standings.update(team_record)
-        standings.update({year: year_standings})
+        standings.update({str(year): year_standings})
     return standings
 
-pprint(get_nba_data())
+# import json
+# nba_data = get_nba_data()
+# with open('data.json', 'w') as outfile:
+#         json.dump(nba_data, outfile)
